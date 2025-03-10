884d2c75a8e3b8109b2e82147fb9601f5914867e
TheSebitzu <sebibonea@gmail.com>
2025-03-10 14:56:10 +0200

    Initial commit

---
+++ src/main/java/net/sebitzu/lolflash/LOLFlashClient.java
@@ -0,0 +1,10 @@
+package net.sebitzu.lolflash;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class LOLFlashClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/net/sebitzu/lolflash/LOLFlashDataGenerator.java
@@ -0,0 +1,11 @@
+package net.sebitzu.lolflash;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class LOLFlashDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
